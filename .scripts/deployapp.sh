2d4739de9398cb54f026365a3147f12536f6adb7
Idris Kausar <kautsaridris@gmail.com>
2022-11-16 22:25:36 +0700

    Update deployapp.sh

--- .scripts/deployapp.sh
+++ .scripts/deployapp.sh
@@ -11,24 +11,6 @@ cp maintenance.html maintenance/
 # Pull the latest version of the app
 git pull origin main
 
-# Install composer dependencies
-# composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader --ignore-platform-req=ext-fileinfo
-
-# Clear the old cache
-# php artisan clear-compiled
-
-# Recreate cache
-# php artisan optimize
-
-# Compile npm assets
-#npm run prod
-
-# Run database migrations
-#php artisan migrate --force
-
-# Exit maintenance mode
-# php artisan up
-
 rm -r maintenance
 
 echo "Deployment finished!"
